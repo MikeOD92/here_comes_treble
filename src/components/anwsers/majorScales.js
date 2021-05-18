5eb23ea24f69fd8b246aa44d37d39b01ca9b40c0
Michael <michaelodell660@gmail.com>
2021-05-17 18:27:39 -0700

    making data set for major scales

---
+++ src/components/anwsers/majorScales.js
@@ -0,0 +1,15 @@
+export default majorScales = {
+    cMajor: ['C','D','E','F', 'G', 'A', 'B', 'C'],
+    dMajor: ['D','E','F#','G','A','B','C#', 'D'],
+    eMajor: ['E','F#', 'G#', 'A', 'B', 'C#', 'D#', 'E'],
+    fMajor: ['F', 'G', 'A', 'A#', 'C', 'D', 'E', 'F' ],
+    gMajor: ['G', 'A', 'B', 'C', 'D', 'E', 'F#', 'G'],
+    aMajor: ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#', 'A'],
+    bMajor: ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#', 'B'],
+    // Cb:[Cb, Db, Eb, Fb, Gb, Ab, Bb, Cb], // no flat everythign is in sharp right now
+    cSharpMajor: ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C', "C#"],
+    dSharpMajor: ['D#', 'F', 'G', 'G#', 'A#', 'C', 'D', 'D#'],
+    fSharpMajor: ['F#', 'G#','A#', 'B', 'C#', 'D#', 'F', 'F#'],
+    gSharpMajor: ['G#', 'A#', 'C', 'C#', 'D#', 'F', 'G', 'G#'],
+    aSharpMajor: ['A#', 'C', 'D', 'D#', 'F', 'G', 'A', 'A#'],
+}
