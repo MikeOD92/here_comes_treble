7f2381d6fab917c19581ff75c1345f212ba97e9f
Michael <michaelodell660@gmail.com>
2021-05-19 09:11:24 -0700

    small tweaks

--- src/components/CustomStaff.js
+++ src/components/CustomStaff.js
@@ -17,17 +17,13 @@ export default function CustomStaff(props) {
                 {props.note === 'D'?(<div className="staffNote d"><img src='https://i.imgur.com/eb2qMJY.png' className="stafficon" /></div>):""}
                 {props.note === 'D#'?(<div className="staffNote d">#<img src='https://i.imgur.com/eb2qMJY.png' className="stafficon" /></div>):""}
             </div>
-
-            {props.note === 'C'?(<div className="staffNote c"><img src='https://i.imgur.com/hTn810D.png' className="stafficon"/></div>):""}
-            {props.note === 'C#'?(<div className="staffNote c">#<img src='https://i.imgur.com/hTn810D.png' className="stafficon"/></div>):""}
-
+                {props.note === 'C'?(<div className="staffNote c"><img src='https://i.imgur.com/hTn810D.png' className="stafficon"/></div>):""}
+                {props.note === 'C#'?(<div className="staffNote c">#<img src='https://i.imgur.com/hTn810D.png' className="stafficon"/></div>):""}
             <div className="staff-line">
                 {props.note === 'B'?(<div className="staffNote b"><img src='https://i.imgur.com/3CbRb0M.png' className="stafficon" /></div>):""}
             </div>
-
-            {props.note === 'A'?(<div className="staffNote a"><img src='https://i.imgur.com/AvLq4aP.png' className="stafficon"/></div>):""}
-            {props.note === 'A#'?(<div className="staffNote a">#<img src='https://i.imgur.com/AvLq4aP.png' className="stafficon"/></div>):""}
-
+                {props.note === 'A'?(<div className="staffNote a"><img src='https://i.imgur.com/AvLq4aP.png' className="stafficon"/></div>):""}
+                {props.note === 'A#'?(<div className="staffNote a">#<img src='https://i.imgur.com/AvLq4aP.png' className="stafficon"/></div>):""}
             <div className="staff-line">
                 {props.note === 'G'?(<div className="staffNote g"><img src='https://i.imgur.com/whLPx2N.png' className="stafficon" /></div>):""}
                 {props.note === 'G#'?(<div className="staffNote g">#<img src='https://i.imgur.com/whLPx2N.png' className="stafficon" /></div>):""}
