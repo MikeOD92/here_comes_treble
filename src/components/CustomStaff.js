4e9e5aa48b3c8de39a27f0be38c4ffd028677a94
Michael <michaelodell660@gmail.com>
2021-05-18 19:47:02 -0700

    added icons to staff and keys

--- src/components/CustomStaff.js
+++ src/components/CustomStaff.js
@@ -6,40 +6,61 @@ export default function CustomStaff(props) {
     return(
         <div id="staff">
             <div className="staff-line">
-                {props.note === 'F'?(<div className="staffNote f">F</div>):""}
-                {props.note === 'F#'?(<div className="staffNote f">#F</div>):""}
+                {props.note === 'F'?(<div className="staffNote f"><img src='https://i.imgur.com/EToiP8v.png' className="stafficon" /></div>):""}
+                {props.note === 'F#'?(<div className="staffNote f">#<img src='https://i.imgur.com/EToiP8v.png' className="stafficon" /></div>):""}
             </div>
                 
-            {props.note === 'E'?(<div className="staffNote e">E</div>):""}
-            {props.note === 'E#'?(<div className="staffNote e">#E</div>):""}
+            {/* {props.note === 'E'?(<div className="staffNote e">E</div>):""}
+            {props.note === 'E#'?(<div className="staffNote e">#E</div>):""} */}
 
             <div className="staff-line">
-                {props.note === 'D'?(<div className="staffNote d">D</div>):""}
-                {props.note === 'D#'?(<div className="staffNote d">#D</div>):""}
+                {props.note === 'D'?(<div className="staffNote d"><img src='https://i.imgur.com/eb2qMJY.png' className="stafficon" /></div>):""}
+                {props.note === 'D#'?(<div className="staffNote d">#<img src='https://i.imgur.com/eb2qMJY.png' className="stafficon" /></div>):""}
             </div>
 
-            {props.note === 'C'?(<div className="staffNote c">C</div>):""}
-            {props.note === 'C#'?(<div className="staffNote c">#C</div>):""}
+            {props.note === 'C'?(<div className="staffNote c"><img src='https://i.imgur.com/hTn810D.png' className="stafficon"/></div>):""}
+            {props.note === 'C#'?(<div className="staffNote c">#<img src='https://i.imgur.com/hTn810D.png' className="stafficon"/></div>):""}
 
             <div className="staff-line">
-                {props.note === 'B'?(<div className="staffNote b">B</div>):""}
+                {props.note === 'B'?(<div className="staffNote b"><img src='https://i.imgur.com/3CbRb0M.png' className="stafficon" /></div>):""}
             </div>
 
-            {props.note === 'A'?(<div className="staffNote a">A</div>):""}
-            {props.note === 'A#'?(<div className="staffNote a">#A</div>):""}
+            {props.note === 'A'?(<div className="staffNote a"><img src='https://i.imgur.com/AvLq4aP.png' className="stafficon"/></div>):""}
+            {props.note === 'A#'?(<div className="staffNote a">#<img src='https://i.imgur.com/AvLq4aP.png' className="stafficon"/></div>):""}
 
             <div className="staff-line">
-                {props.note === 'G'?(<div className="staffNote g">G</div>):""}
-                {props.note === 'G#'?(<div className="staffNote g">#G</div>):""}
+                {props.note === 'G'?(<div className="staffNote g"><img src='https://i.imgur.com/whLPx2N.png' className="stafficon" /></div>):""}
+                {props.note === 'G#'?(<div className="staffNote g">#<img src='https://i.imgur.com/whLPx2N.png' className="stafficon" /></div>):""}
             </div>
 
-                {props.note === 'F'?(<div className="staffNote f">F</div>):""}
-                {props.note === 'F#'?(<div className="staffNote f">#F</div>):""}
+                {/* {props.note === 'F'?(<div className="staffNote f"><img src='https://i.imgur.com/EToiP8v.png' className="stafficon" /></div>):""}
+                {props.note === 'F#'?(<div className="staffNote f">#<img src='https://i.imgur.com/EToiP8v.png' className="stafficon" /></div>):""} */}
 
             <div className="staff-line">
-                {props.note === 'E'?(<div className="staffNote e">E</div>):""}
-                {props.note === 'E#'?(<div className="staffNote e">#E</div>):""}
+                {props.note === 'E'?(<div className="staffNote e"><img src='https://i.imgur.com/1G7H6cv.png' className="stafficon" /></div>):""}
+                {props.note === 'E#'?(<div className="staffNote e">#<img src='https://i.imgur.com/1G7H6cv.png' className="stafficon" /></div>):""}
             </div>
         </div>
     )
 }
+
+
+
+//         case "F":
+//             return(<img src='https://i.imgur.com/EToiP8v.png' className="icon" />) 
+//             break;
+//         case "F#":
+//             return(<div><img src='https://i.imgur.com/EToiP8v.png' className="icon sharpIcon"/>#</div>)
+//             break;
+
+//         case "G":
+//             return(<img src='https://i.imgur.com/whLPx2N.png' className="icon" />) 
+//             break;
+//         case "G#":
+//             return(<div><img src='https://i.imgur.com/whLPx2N.png' className="icon sharpIcon" />#</div>)
+//             break;
+//         case "B":
+//             return() 
+//             break;
+//     }
+// }
--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Switch } from 'react-router';
 import * as Tone from "tone";
 
 
@@ -11,19 +12,67 @@ export default function Keys(props){
 
     const notesArr = ['C','C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
     
-    const sound = () => {
-        synth.triggerAttackRelease(`${note}${pitch}`, "8n");
+    const sound = (e) => {
+        e.preventDefault();
+        synth.triggerAttack(`${note}${pitch}`);
         if(note === props.testNote){
                 // setTimeout(() => alert('You Clicked the right note'), 300);
                 props.setiterator(iterator += 1)
             }else{
-                setTimeout(() => alert('You Clicked the wrong note try again'), 300);
+                // setTimeout(() => alert('You Clicked the wrong note try again'), 300);
+                // alert(e.target[0])
+                console.log(e.target)
+                
             }
+            synth.triggerRelease("+0.25");
         
     }
+    const img = (note) =>{
+        switch(note){
+            case "A":
+                return(<img src='https://i.imgur.com/AvLq4aP.png' className="icon " />) 
+                break;
+            case "A#":
+                return(<div><img src='https://i.imgur.com/AvLq4aP.png'className="icon sharpIcon "/>#</div>)
+                break;
+            case "C":
+                return(<img src='https://i.imgur.com/hTn810D.png' className="icon "/>) 
+                break;
+            case "C#":
+                return(<div><img src='https://i.imgur.com/hTn810D.png' className="icon sharpIcon"/>#</div>)
+                break;
+            case "D":
+                return(<img src='https://i.imgur.com/eb2qMJY.png' className="icon" />) 
+                break;
+            case "D#":
+                return(<div><img src='https://i.imgur.com/eb2qMJY.png' className="icon sharpIcon"/>#</div>)
+                break;
+            case "E":
+                return(<img src='https://i.imgur.com/1G7H6cv.png' className="icon" />) 
+                break;
+            case "E#":
+                return(<div><img src='https://i.imgur.com/1G7H6cv.png' className="icon sharpIcon"/>#</div>)
+                break;
+            case "F":
+                return(<img src='https://i.imgur.com/EToiP8v.png' className="icon" />) 
+                break;
+            case "F#":
+                return(<div><img src='https://i.imgur.com/EToiP8v.png' className="icon sharpIcon"/>#</div>)
+                break;
 
+            case "G":
+                return(<img src='https://i.imgur.com/whLPx2N.png' className="icon" />) 
+                break;
+            case "G#":
+                return(<div><img src='https://i.imgur.com/whLPx2N.png' className="icon sharpIcon" />#</div>)
+                break;
+            case "B":
+                return(<img src='https://i.imgur.com/3CbRb0M.png' className="icon" />) 
+                break;
+        }
+    }
     return(
-        <div className={`key ${keyType}`} onClick={() => sound()}>{note}</div>
+        <div className={`key ${keyType}`} onClick={(e) => sound(e)}>{img(note)}</div>
     )
 }
 
