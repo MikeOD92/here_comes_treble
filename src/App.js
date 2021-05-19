944f9d924bc94f07a1ad468fabe5f947835f53c8
cobycobyk <coby@robo3d.com>
2021-05-18 18:37:07 -0600

    add user oauth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+import { getUser } from './utilities/users-service';
 import "./App.css";
+import AuthPage from "./pages/AuthPage";
 import Gamepage from './pages/Gamepage';
 
 export default function App() {
 
+  const [user, setUser] = useState(getUser());
+
   return (
     <div className="App">
       <Gamepage/>
+      <AuthPage setUser={setUser} />
+      {user ? 
+      (<div>logged in</div> )
+      : 
+      (<div>logged out</div>)
+      }
     </div>
   );  
 }
